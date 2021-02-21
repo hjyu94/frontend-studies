f19ea2b472fe2f44984905bb053e93a934ddb3e9
Hyojeong-Yu <hjeong@lunit.io>
2021-02-21 21:47:30 +0900

    [이벤트] 컴포넌트 이벤트 만들기

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -35,21 +35,16 @@ class App extends Component {
 
     return (
       <div className="App">
-        {/* <Subject 
+        <Subject 
           title={this.state.subject.title} 
-          sub={this.state.subject.sub}>
-        </Subject> */}
-        <header>
-          <h1><a href="/" onClick={function(e){
-            console.log(e);
-            e.preventDefault();
-            this.state.mode = 'welcome';
+          sub={this.state.subject.sub}
+          onChangePage={function(){
             this.setState({
               mode: 'read'
             });
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.sub}
-        </header>
+          }.bind(this)}
+        >
+        </Subject>
         <TOC data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
