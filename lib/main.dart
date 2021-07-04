9dcb39ea7af51addf28aa6a8c27ba675dfea619d
Hyojeong-Yu <hjeong@lunit.io>
2021-07-04 15:20:23 +0900

    Use an external package

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
+// The main() method uses arrow (=>) notation. Use arrow notation for one-line functions or methods.
 void main() => runApp(MyApp());
-  
 
+// The app extends StatelessWidget, which makes the app itself a widget.
+// In Flutter, almost everything is a widget, including alignment, padding, and layout.
 class MyApp extends StatelessWidget {
+  final wordPair = WordPair.random();
+
+  // A widget’s main job is to provide a build() method
+  // that describes how to display the widget in terms of other, lower level widgets.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      // The Scaffold widget, from the Material library,
+      // provides a default app bar, and a body property
+      // that holds the widget tree for the home screen.
+      // The widget subtree can be quite complex.
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: Text("Hello My first app"),
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        )
       ),
     );
   }
 }
+
