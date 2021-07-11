7e3a4d3d32e5c69b2de62dc4803fdf58395b710a
Hyojeong-Yu <hjeong@lunit.io>
2021-07-11 15:23:00 +0900

    [login dice] TextEditController, Navigator.push

--- lib/login_dice/main.dart
+++ lib/login_dice/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dice.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -19,6 +21,10 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
+
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,70 +43,95 @@ class _LogInState extends State<LogIn> {
           )
         ],
       ),
-      body: SingleChildScrollView( // 키보드가 올라와서 화면을 가릴 수 있음, 키보드가 올라온 영역 만큼 스크롤 해주는 위젯
-        child: Column(
-          children: [
-            Center(
-              child: Image(
-                image: AssetImage('image/chef.gif'),
-                width: 170.0,
-                height: 190.0,
-              )
-            ),
-            Form(
-              child: Theme(
-                data: ThemeData(
-                  primaryColor: Colors.teal,
-                  inputDecorationTheme: InputDecorationTheme(
-                    labelStyle: TextStyle(
-                      color: Colors.teal,
-                      fontSize: 15.0,
-                    )
+      body: Builder(
+        builder: (context) {
+          return SingleChildScrollView( // 키보드가 올라와서 화면을 가릴 수 있음, 키보드가 올라온 영역 만큼 스크롤 해주는 위젯
+            child: Column(
+              children: [
+                Center(
+                  child: Image(
+                    image: AssetImage('image/chef.gif'),
+                    width: 170.0,
+                    height: 190.0,
                   )
                 ),
-                child: Container(
-                  padding: EdgeInsets.all(40.0),
-                  child: Column(
-                    children: <Widget>[
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter "dice"',
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter Password',
-                        ),
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                      ),
-                      SizedBox(height: 40.0), // blank between text field and button theme
-                      ButtonTheme(
-                        minWidth: 100.0,
-                        height: 50.0,
-                        child: ElevatedButton(
-                          onPressed: () {
-
-                          },
-                          child: Icon(Icons.arrow_forward,
-                            color: Colors.white,
-                            size: 35.0,
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.orangeAccent,
-                          ),
-                        ),
+                Form(
+                  child: Theme(
+                    data: ThemeData(
+                      primaryColor: Colors.teal,
+                      inputDecorationTheme: InputDecorationTheme(
+                        labelStyle: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 15.0,
+                        )
                       )
-                    ],
-                  ),
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(40.0),
+                      child: Column(
+                        children: <Widget>[
+                          TextField(
+                            controller: controller,
+                            decoration: InputDecoration(
+                              labelText: 'Enter "dice"',
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          TextField(
+                            controller: controller2,
+                            decoration: InputDecoration(
+                              labelText: 'Enter Password',
+                            ),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          SizedBox(height: 40.0), // blank between text field and button theme
+                          ButtonTheme(
+                            minWidth: 100.0,
+                            height: 50.0,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if(controller.text == 'dice' && controller2.text == '1234') {
+                                  Navigator.push(context, MaterialPageRoute(
+                                    builder: (BuildContext context) => Dice()
+                                  ));
+                                } else if(controller.text == 'dice' && controller2.text != '1234') {
+                                  showSnackBar(context, '비밀번호가 일치하지 않습니다');
+                                } else if(controller.text != 'dice' && controller2.text == '1234') {
+                                  showSnackBar(context, 'dice 철자를 확인하세요');
+                                } else {
+                                  showSnackBar(context, '로그인 정보를 확인하세요');
+                                }
+                              },
+                              child: Icon(Icons.arrow_forward,
+                                color: Colors.white,
+                                size: 35.0,
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.orangeAccent,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  )
                 )
-              )
-            )
-          ],
-        ),
-      ),
+              ],
+            ),
+          );
+        }
+      )
     );
   }
 }
 
+void showSnackBar(BuildContext context, String msg) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(msg,
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
