3023e1c676575e7eccd07224fc433bb7e7d5a725
Hyojeong-Yu <hjeong@lunit.io>
2021-07-11 11:49:40 +0900

    Snackbar - 다른 화면으로 넘어가는 경우 스낵바 닫도록 하기

--- lib/main5.dart
+++ lib/main5.dart
@@ -86,30 +86,38 @@ class ThirdPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Third Page'),
+    return ScaffoldMessenger(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Third Page'),
+        ),
+        body: Builder(
+          builder: (context) {
+            return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('좋아요가 취소하시겠습니까?',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('좋아요가 취소되었습니다'),
+                                duration: Duration(seconds: 3),
+                              ));
+                        },
+                        child: Text('취소하기')
+                    )
+                  ],
+                )
+            );
+          }
+        ),
       ),
-      body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('좋아요가 취소하시겠습니까?',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.redAccent,
-                ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('좋아요가 취소되었습니다'),
-                      duration: Duration(seconds: 3),
-                    ));
-                  },
-                  child: Text('취소하기')
-              )
-            ],
-          )),
     );
   }
 }
