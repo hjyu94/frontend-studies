6450b4d5f387034afd15be66fe9c748467e8f5ff
Hyojeong-Yu <hjeong@lunit.io>
2021-07-11 15:36:38 +0900

    [LOGIN DICE] AssetImage (dice1.png 화면 가운데 정렬)

--- lib/login_dice/dice.dart
+++ lib/login_dice/dice.dart
@@ -5,8 +5,27 @@ class Dice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text('Dice game'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // 주축 정렬
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center, // 주축 정렬
+              children: [
+                Image(image: AssetImage('image/dice1.png'),
+                  width: 300.0,
+                )
+              ],
+            )
+          ],
+        )
+      ),
     );
   }
 }
