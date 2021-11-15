40f4c876431ff5e0fa96cd27d7c281d08e6a302f
Hyojeong Yu <hjeong@lunit.io>
2021-11-15 23:26:36 +0900

    TodoList 에서 state 의 todos 조회하여 렌더링

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import TodoItem from './TodoItem';
+import { useTodoState } from '../TodoContext';
 
 const TodoListBlock = styled.div`
   flex: 1; /* 자신이 차지 할 수 있는 영역을 꽉 채우도록 설정 */
@@ -9,15 +10,23 @@ const TodoListBlock = styled.div`
   overflow-y: auto;
 `;
 
+// state 를 조회해서 todo item 렌더링
+// onToggle, onRemove 와 같은 작업은 TodoList 에서 할 필요 없고, TodoItem 에서 작업할 예정
 function TodoList() {
-  return (
-    <TodoListBlock>
-        <TodoItem text="프로젝트 생성하기" done={true} />
-        <TodoItem text="컴포넌트 스타일링 하기" done={true} />
-        <TodoItem text="Context 만들기" done={false} />
-        <TodoItem text="기능 구현하기" done={false} />
-    </TodoListBlock>
-  );
-}
+    const todos = useTodoState();
+
+    return (
+      <TodoListBlock>
+        {todos.map(todo => (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            text={todo.text}
+            done={todo.done}
+          />
+        ))}
+      </TodoListBlock>
+    );
+  }
 
 export default TodoList;
