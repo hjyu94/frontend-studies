9a00595a683c73448363c475ee76ffb31fe49bc4
Hyojeong Yu <hjeong@lunit.io>
2021-11-15 23:16:24 +0900

    컴포넌트 TodoProvider 로 감싸기

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoHead from './components/TodoHead';
 import TodoList from './components/TodoList';
 import TodoCreate from './components/TodoCreate';
+import { TodoProvider } from './TodoContext';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -12,16 +13,20 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+
 function App() {
   return (
-    <div>
+    <TodoProvider
+      // 우리 프로젝트 모든 곳에서 Todo 관련 Context 들을 사용 할 수 있도록,
+      // App 컴포넌트에서 TodoProvider 를 불러와서 모든 내용을 TodoProvider 로 감싸주겠습니다.
+    >
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
         <TodoList />
         <TodoCreate />
       </TodoTemplate>
-    </div>
+    </TodoProvider>
   );
 }
 
